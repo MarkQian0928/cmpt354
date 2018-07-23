945b151f5a30046d21f86c8e488334b0737002e9
Mark Qian <markqian199413@gmail.com>
2018-07-23 11:12:03 -0700

    first attemp to updte account info

--- users/views.py
+++ users/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class UpdateInfo(generic.CreateView):
+    form_class = CustomUserChangeForm
+    template_name = 'updateInfo.html'
