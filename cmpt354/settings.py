10e12edb26f8f18d232ed4ed6a9b84844b78ddc6
Mark Qian <markqian199413@gmail.com>
2018-07-26 11:14:31 -0700

    projection, selection, aggregation, nested aggregation, delete, done

--- cmpt354/settings.py
+++ cmpt354/settings.py
@@ -32,11 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
-<<<<<<< HEAD
-    'Cart',
-=======
+    # 'Cart',
     'shoes',
->>>>>>> Shoes
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
